6f255e83e8a1d7ecf23df613061152b10f3d1052
nathan <nathan@vermeiren.org>
2026-05-03 14:31:33 +0200

    extra tests for exit codes

--- tests/test_exit_code.pl
+++ tests/test_exit_code.pl
@@ -60,4 +60,17 @@ test(run_exit_invalid_is_one) :-
     run_cli_exit(['run', TempFile], Status),
     assertion(Status == exit(1)).
 
+
+test(run_exit_invalid_local_get_is_one) :-
+    TempFile = '/tmp/test_exit_invalid_local.pwat',
+    write_test_file(TempFile,
+        '(module
+          (start 0)
+          (func (args 0) (locals 0) (results 1)
+            local.get 0
+          )
+        )'),
+    run_cli_exit(['run', TempFile], Status),
+    assertion(Status == exit(1)).
+
 :- end_tests(exit_codes).
